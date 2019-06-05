673a4e3e3fe8b19f09c958f9dc6940aa1943cf4a
Ryan <ryan.j.z@hotmail.com>
2019-06-05 14:09:47 +0200

    update test skiptrack

--- src/test/java/emily/command/test/SkipTrackTest.java
+++ src/test/java/emily/command/test/SkipTrackTest.java
@@ -1,13 +1,44 @@
 package emily.command.test;
 
+import emily.command.meta.CommandVisibility;
+import emily.command.music.SkipTrack;
 import emily.permission.SimpleRank;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SkipTrackTest {
 
+    private SkipTrack skiptrack = new SkipTrack();
+
+    @Test
+    public void testDescription() {
+        assertEquals("skip current track", skiptrack.getDescription());
+    }
+
+    @Test
+    public void testCommand() {
+        assertEquals("skip", skiptrack.getCommand());
+    }
+
+    @Test
+    public void testUsage() {
+        assertNotNull(skiptrack.getUsage());
+    }
+
+    @Test
+    public void testVisibility() {
+        assertEquals(skiptrack.getVisibility(), CommandVisibility.PUBLIC);
+
+    }
+
+    @Test
+    public void testAliases() {
+        assertArrayEquals(skiptrack.getAliases(), new String[]{"next"});
+    }
+
     @Test
     public void executeSkipSong() {
+
         SimpleRank userRank = SimpleRank.GUILD_ADMIN;
         boolean adminOnly = true;
 
